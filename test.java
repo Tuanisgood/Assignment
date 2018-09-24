d9f0e36b367bd39dc78f5433af66cbaff0bed19d
Johnny Kohi Bui <43369521+Tuanisgood@users.noreply.github.com>
2018-09-24 17:05:26 +0700

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 class test{
 	public static void main (String[] args){
-		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+                String n = sc.nextLine();
+                System.out.printf("Hello World my name is "+n);
 	}
 }
